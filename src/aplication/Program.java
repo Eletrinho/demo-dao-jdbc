f08f1c0e21903b2e1d97f68f3ac068a0f9297385
Cauã <cauacustodiopereira@gmail.com>
2024-09-20 13:42:01 -0300

    Seller class created

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,14 @@
 package aplication;
 
 import entities.model.Department;
+import entities.model.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000d, obj);
+        System.out.println(seller);
     }
 }
